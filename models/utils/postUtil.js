a9e8e6b9686318283e81b3b23be784192259d34a
Dogl4 <doougllas@hotmail.com.br>
2022-02-12 13:50:48 -0300

    :small_blue_diamond: 8.1 Corrigi endpoint GET `/post`, traz todas as categorias agora, no array.

--- models/utils/postUtil.js
+++ models/utils/postUtil.js
@@ -10,20 +10,19 @@ const createPost = async (categoryIds, post) => {
 };
 
 const getAllPostsClean = async () => {
-  const allPostsCategory = await PostsCategory.findAll({ raw: true });
-  const allPosts = await Promise.all(allPostsCategory.map(async ({ postId, categoryId }) => {
-    const postClean = await BlogPost.findOne({ where: { id: postId }, raw: true });
-  
-    const categoryClean = await Category.findOne({ where: { id: categoryId }, raw: true });
-  
-    const categoryByUser = await BlogPost.findOne({
-       where: { id: postId }, include: { model: User, as: 'user' } });
-    const userClean = categoryByUser.dataValues.user.dataValues;
-  
-    return ({ ...postClean, user: userClean, categories: [categoryClean] });
-  }));
+  const allBlogPost = await BlogPost.findAll({ 
+    include: [
+      { as: 'user', model: User, attributes: { exclude: ['password'] } },
+      { as: 'categories', model: Category },
+    ],
+  });
 
-  return allPosts;
+  const newAllPost = allBlogPost.map(({ dataValues }) => ({
+    ...dataValues,
+    user: dataValues.user.dataValues,
+    categories: (dataValues.categories.map(({ id, name }) => ({ id, name }))),
+  }));
+  return newAllPost;
 };
 
 const getPostByIdClean = async (id) => {
