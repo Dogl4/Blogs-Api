cdea03a68eb17bbba2e1ba25a595ba95d55d4998
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-08-03 09:15:22 -0300

    🐛 fix(`user`): now can more length in password
    update `login.js` and `user.js`

--- schemas/login.js
+++ schemas/login.js
@@ -2,5 +2,5 @@ const Joi = require('joi');
 
 module.exports = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).required(),
 });
--- schemas/user.js
+++ schemas/user.js
@@ -3,6 +3,6 @@ const Joi = require('joi');
 module.exports = Joi.object({
   displayName: Joi.string().min(8),
   email: Joi.string().email().required(),
-  password: Joi.string().length(6).required(),
+  password: Joi.string().min(6).required(),
   image: Joi.string(),
 });
