93488c724da20a32b02498d67e048e23931b4d7a
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-08-04 08:36:49 -0300

    ♻️ refactor(`userController`):  `registerUser`
    moved validate to schema and routes

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,8 +1,14 @@
 const Joi = require('joi');
 
-module.exports = Joi.object({
+const userSchema = (objectUser) => Joi.object({
   displayName: Joi.string().min(8),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
   image: Joi.string(),
-});
+}).validate(objectUser);
+
+module.exports = (req, _res, next) => {
+  const { error } = userSchema(req.body);
+  if (error) throw error;
+  next();
+};
