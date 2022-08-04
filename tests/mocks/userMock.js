8e4037b4e56f40a869da6cb22ddd273e44e4283c
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-08-04 14:55:03 -0300

    ✅ test: create test to function `getAllUsers`
    update userMock.js and userController.test.js

--- tests/mocks/userMock.js
+++ tests/mocks/userMock.js
@@ -7,8 +7,25 @@ const userMock = {
   },
   status: {
     CREATED: 201,
+    OK: 200,
   },
-  token: 'a56465889684fdsdfds834f65¨$$^.nmjh'
+  token: 'a56465889684fdsdfds834f65¨$$^.nmjh',
+  list: [
+          {
+              "id": 1,
+              "displayName": "Lewis Hamilton",
+              "email": "lewishamilton@gmail.com",
+              "password": "123456",
+              "image": "https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg"
+          },
+          {
+              "id": 2,
+              "displayName": "Michael Schumacher",
+              "email": "MichaelSchumacher@gmail.com",
+              "password": "123456",
+              "image": "https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg"
+          }
+        ]
 }
 
 module.exports = userMock;
--- tests/unit/controllers/userController.test.js
+++ tests/unit/controllers/userController.test.js
@@ -1,29 +1,26 @@
 const { expect } = require('chai');
-const { before } = require('mocha');
+const { before, after } = require('mocha');
 const Sinon = require('sinon');
 
-const { registerUser } = require('../../../src/controllers/userController');
+const { registerUser, getAllUsers } = require('../../../src/controllers/userController');
 const { userService } = require('../../../src/services');
 const { userMock } = require('../../mocks');
 
 describe('UserController', () => {
-  const req = {}
-  const res = {}
-
-  before(() => {
-    res.status = Sinon.stub().returns(res);
-    res.json = Sinon.stub();
-  })
-
   describe('#registerUser', () => {
+    const req = {}
+    const res = {}
+
     before(() => {
+      res.status = Sinon.stub().returns(res);
       req.body = userMock.body;
+      res.json = Sinon.stub();
       Sinon.stub(userService, 'createUser').returns(userMock.token);
     })
 
     it('should return status code 201', async () =>{
       await registerUser(req, res, () => {});
-      expect(res.status.calledWith(201)).to.be.true;
+      expect(res.status.calledWith(userMock.status.CREATED)).to.be.true;
     })
 
     it('should return a token', async () => {
@@ -33,7 +30,28 @@ describe('UserController', () => {
     })
   })
 
-  describe('#getAllUsers', () => {})
+  describe('#getAllUsers', () => {
+    const req = {}
+    const res = {}
+
+    before(() => {
+      res.status = Sinon.stub().returns(res);
+      res.json = Sinon.stub();
+      Sinon.stub(userService, 'getAll').returns(userMock.list);
+    })
+
+    it('should return status code 200', async () => {
+      await getAllUsers(req, res, () => {});
+      expect(userService.getAll.called).to.have.been.true;
+      expect(res.status.calledWith(userMock.status.OK)).to.be.true;
+    })
+
+    it('should return an array of users', async () => {
+      await registerUser(req, res, () => {});
+      expect(res.json.args[0][0]).to.be.an('array');
+      expect(res.json.args[0][0]).to.be.equal(userMock.list);
+    })
+  })
 
   describe('#getByIdUser', () => {})
 
