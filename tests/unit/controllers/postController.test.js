d79abb05a6c92bdee4ae5a6603050dc003829f98
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-08-05 07:54:01 -0300

    ✅ test: create test to function `getAllPosts`
    update postController.test.js

--- tests/unit/controllers/postController.test.js
+++ tests/unit/controllers/postController.test.js
@@ -4,6 +4,7 @@ const Sinon = require('sinon');
 
 const {
   registerPost,
+  getAllPosts,
 } = require('../../../src/controllers/postController');
 const { postService } = require('../../../src/services');
 const { postMock, statusMock } = require('../../mocks');
@@ -28,6 +29,29 @@ describe('Post Controller', () => {
     it('should return a post', async () => {
       await registerPost(req, res, () => {});
       expect(res.json.args[0][0]).to.be.equal(postMock.list[2]);
+    })
+  })
+
+    describe('#getAllPosts', () => {
+    const req = {}
+    const res = {}
+
+    before(() => {
+      res.status = Sinon.stub().returns(res);
+      res.json = Sinon.stub();
+      Sinon.stub(postService, 'getAllPosts').returns(postMock.list);
+    })
+
+    it('should return status code 200', async () => {
+      await getAllPosts(req, res, () => {});
+      expect(postService.getAllPosts.called).to.have.been.true;
+      expect(res.status.calledWith(statusMock.OK)).to.be.true;
+    })
+
+    it('should return an array of posts', async () => {
+      await getAllPosts(req, res, () => {});
+      expect(res.json.args[0][0]).to.be.an('array');
+      expect(res.json.args[0][0]).to.be.equal(postMock.list);
     })
   })
 })
